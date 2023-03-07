4a212256d5ae06c45cf6f1889205e4a8ed6ab566
firstaple <firstaple@gmail.com>
2023-03-07 14:46:08 +0900

    Events part Two

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const loginForm = document.querySelector("#Login-form");
 const loginInput = document.querySelector("#Login-form input");
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  alert("clicked");
+}
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
